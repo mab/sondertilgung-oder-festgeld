caa642340cb9307768c7a89e9d87410f44614ad5
Matthias Baumgart <matthias@briefhansa.de>
2025-01-02 11:41:39 +0100

    Add deploy workflow to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/sondertilgung-oder-festgeld",
 };
 
 export default nextConfig;
